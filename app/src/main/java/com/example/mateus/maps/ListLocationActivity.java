bd0aa246262f46406a1c980ac01cf3f6e1bd6b56
Mateus <costagnr@hotmail.com>
2015-10-09 15:13:58 -0300

    Context menu

--- app/src/main/java/com/example/mateus/maps/ListLocationActivity.java
+++ app/src/main/java/com/example/mateus/maps/ListLocationActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,14 +18,14 @@ import java.util.ArrayList;
 public class ListLocationActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ArrayList<Lugar> lugares;
-    private ListView listview;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_location);
         //getActionBar().setDisplayHomeAsUpEnabled(true);
-Log.d("ListloCATIONacTIVITY", "onCreate");
+
         lugares = LocationManager.getInstance().getLugares();
         if (savedInstanceState != null) {
             final ArrayList<Lugar> tmp = savedInstanceState.getParcelableArrayList(MainActivity.LIST_LOCATION);
@@ -31,19 +33,20 @@ Log.d("ListloCATIONacTIVITY", "onCreate");
                 LocationManager.getInstance().setLugares(tmp);
         }
 
-        listview = (ListView) findViewById(R.id.listView);
+        listView = (ListView) findViewById(R.id.listView);
 
         //final ListAdapter adapter = new LocationArrayAdapter(this, lugares);
         //final StableArrayAdapter adapter = new StableArrayAdapter(this, android.R.layout.simple_list_item_1, lugares);
         final LocationBaseAdapter adapter = new LocationBaseAdapter(this, lugares);
-        listview.setAdapter(adapter);
-        listview.setOnItemClickListener(this);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+        registerForContextMenu(listView);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        ((BaseAdapter)listview.getAdapter()).notifyDataSetChanged();
+        ((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
     }
 
     @Override
@@ -63,6 +66,8 @@ Log.d("ListloCATIONacTIVITY", "onCreate");
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_help) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -77,10 +82,51 @@ Log.d("ListloCATIONacTIVITY", "onCreate");
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        editLocation(position);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if (v.getId() == R.id.listView) {
+            ListView lv = (ListView) v;
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            Lugar obj = (Lugar) lv.getItemAtPosition(acmi.position);
+
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.context_menu, menu);
+
+            menu.setHeaderTitle(obj.getNome());
+
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.edit:
+                editLocation((int) info.id);
+                return true;
+            case R.id.delete:
+                removeLocation((int) info.id);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void editLocation(int position) {
         //Toast.makeText(this, ((Lugar) parent.getItemAtPosition(position)).getNome(), Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, EditLocationActivity.class);
         intent.putExtra(MainActivity.EDIT_LOCATION, position);
         startActivity(intent);
     }
 
+    private void removeLocation(int position) {
+        //Toast.makeText(this, ((Lugar) parent.getItemAtPosition(position)).getNome(), Toast.LENGTH_SHORT).show();
+        lugares.remove(position);
+        ((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
+    }
 }
